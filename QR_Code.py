7b0e2480d3feb5d255abdeb422274083db6287eb
Mathis ALLOUCHE <mathis.allouche@ens.uvsq.fr>
2022-03-29 16:45:17 +0200

    tempo

--- QR_Code.py
+++ QR_Code.py
@@ -1,6 +1,4 @@
-import PIL as pil
 from PIL import Image
-# from PIL import ImageTk
 
 
 # Fonctions
@@ -13,9 +11,9 @@ def nbrCol(mat):
 
 
 def saving(matPix, filename):
-    # sauvegarde l'image contenue dans matpix dans le fichier filename
-    # utiliser une extension png pour que la fonction fonctionne sans perte d'information
-    toSave = pil.Image.new(mode="1", size=(nbrCol(matPix), nbrLig(matPix)))
+    ''' sauvegarde l'image contenue dans matpix dans le fichier filename
+        utiliser une extension png pour que la fonction fonctionne sans perte d'information'''
+    toSave = Image.new(mode="1", size=(nbrCol(matPix), nbrLig(matPix)))
     for i in range(nbrLig(matPix)):
         for j in range(nbrCol(matPix)):
             toSave.putpixel((j, i), matPix[i][j])
@@ -25,7 +23,7 @@ def saving(matPix, filename):
 def loading(filename):
     # charge le fichier image filename et renvoie une matrice de 0 et de 1 qui représente
     # l'image en noir et blanc
-    toLoad = pil.Image.open(filename)
+    toLoad = Image.open(filename)
     mat = [[0] * toLoad.size[0] for _ in range(toLoad.size[1])]
     for i in range(toLoad.size[1]):
         for j in range(toLoad.size[0]):
@@ -34,6 +32,7 @@ def loading(filename):
 
 
 def coin_QC():
+    '''TO DO'''
     mat = [[0] * 7 for _ in range(7)]
 
     for i in range(7):
@@ -44,15 +43,16 @@ def coin_QC():
 
 
 def rotation(sens, mat):
+    '''TO DO'''
     if sens == 0:
         return [[mat[-(i + 1)][j] for i in range(nbrLig(mat))] for j in range(nbrCol(mat))]
     elif sens == 1:
         return [[mat[i][-(j + 1)] for i in range(nbrLig(mat))] for j in range(nbrCol(mat))]
-
     return [[mat[-(i + 1)][-(j + 1)] for i in range(nbrLig(mat))] for j in range(nbrCol(mat))]
 
 
 def verif_sens_QC(mat):
+    '''TO DO'''
     coin = coin_QC()
     for i in range(7):
         for j in range(7):
@@ -70,6 +70,28 @@ def verif_sens_QC(mat):
 
 def verif_():
     pass
+
+
+def correction_hamming(liste):
+    m1, m2, m3, m4, c1, c2, c3 = liste
+    p_err = []
+    if c1 == (m1 + m2 + m4) % 2:
+        p_err.append(3)
+    if c2 == (m1 + m3 + m4) % 2:
+        p_err.append(2)
+    if c3 == (m2 + m3 + m4) % 2:
+        p_err.append(1)
+
+    if len(p_err) == 0:
+        return liste
+    elif len(p_err) == 1:
+        pass
+    elif len(p_err) == 2:
+        pass
+    elif len(p_err) > 2:
+        return False
+
+    print(p_err)
 
 
 # variables
