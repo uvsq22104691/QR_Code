a137c6c39c815dbdc7b16d0654d471f2eff4e8e3
Mathis ALLOUCHE <mathis.allouche@ens.uvsq.fr>
2022-03-30 23:16:49 +0200

    début creer QR

--- QR_Code.py
+++ QR_Code.py
@@ -1,4 +1,4 @@
-from PIL import Image
+from PIL import Image, ImageTk
 import tkinter as tk
 import tkinter.filedialog as fd
 import os
@@ -28,11 +28,18 @@ def saving(matPix, filename):
 def loading(filename):
     # charge le fichier image filename et renvoie une matrice de 0 et de 1 qui représente
     # l'image en noir et blanc
+    global L_image, img
     toLoad = Image.open(filename)
+
     mat = [[0] * toLoad.size[0] for _ in range(toLoad.size[1])]
     for i in range(toLoad.size[1]):
         for j in range(toLoad.size[0]):
             mat[i][j] = 0 if toLoad.getpixel((j, i)) == 0 else 1
+
+    toLoad = toLoad.resize((300, 300))
+    img = ImageTk.PhotoImage(toLoad)
+    L_image['image'] = img
+
     return mat
 
 
@@ -123,8 +130,14 @@ def correction_hamming(liste):
     return liste[:4]
 
 
-def lire_qr_code(mat):
+def extraire_donnee(mat):
     '''TO DO'''
+    return [ligne[-14:] for ligne in mat[-16:]]
+
+
+def lire_donnee(mat):
+    '''TO DO'''
+    global L_message
     liste = []
     for i in range(0, 16, 2):
         m = mat[::-1][i:i + 2][::-1]
@@ -136,7 +149,6 @@ def lire_qr_code(mat):
         else:
             m1 = [m[(i + 1) % 2][i // 2] for i in range(0, 14)]
             m2 = [m[(i + 1) % 2][i // 2 + 7] for i in range(0, 14)]
-            pass
 
         liste.append(m1)
         liste.append(m2)
@@ -145,28 +157,39 @@ def lire_qr_code(mat):
 
 def lire_type_donnee(mat):
     '''TO DO'''
-    return mat[24][8]
+    type = mat[24][8]
+    return type
 
 
-def interpreter_ascii(mat):
+def interpreter_ascii(data):
     '''TO DO'''
-    for bloc in [liste for liste in mat if sum(liste) != 14]:
+    global L_info
+    s = ""
+    for bloc in [liste for liste in data if sum(liste) != 14]:
         tmp = ''.join(map(str, correction_hamming(bloc[:7]) + correction_hamming(bloc[7:])))[::-1]
         tmp = int(tmp, 2)
-        print(chr(tmp), end="")
+        s += chr(tmp)
+    L_info['text'] = L_info['text'] + f"\nMessage:\t\t{s}"
 
 
-def interpreter_num(mat):
+def interpreter_num(data):
     '''TO DO'''
-    for bloc in [liste for liste in mat if sum(liste) != 14]:
+    global L_info
+    s = ""
+    for bloc in [liste for liste in data if sum(liste) != 14]:
         tmp = ''.join(map(str, correction_hamming(bloc[:7]) + correction_hamming(bloc[7:])))[::-1]
-        print(hex(int(tmp[:4], 2))[2:3], hex(int(tmp[4:], 2))[2:3], sep="", end=" ")
+        s += hex(int(tmp[:4], 2))[2:3] + hex(int(tmp[4:], 2))[2:3] + " "
+    s = s[:-1]
+    L_info['text'] = L_info['text'] + f"\nMessage:\t\t{s}"
 
 
-def interpreter(mat, data):
-    if lire_type_donnee(mat) == 1:
+def interpreter(data, dataType):
+    global L_info
+    if dataType == 1:
+        L_info['text'] = L_info['text'] + "\nType:\t\tascii"
         interpreter_ascii(data)
     else:
+        L_info['text'] = L_info['text'] + "\nType:\t\tnumérique"
         interpreter_num(data)
 
 
@@ -176,50 +199,166 @@ def lire_type_filtre(mat):
     return int(filtre, 2)
 
 
-def applique_filtre(matrice):
+def applique_filtre(data, filtre):
     '''TO DO'''
-    filtre = lire_type_filtre(matrice)
+    global L_info
     if filtre == 0:
-        return matrice
+        L_info['text'] = "Filtre:\t\taucun filtre"
+        return data
     elif filtre == 1:
-        for y in range(-16, 0, 1):
-            for x in range(-14, 0, 1):
-                matrice[y][x] = matrice[y][x] ^ (y + x) % 2
-        return matrice
+        L_info['text'] = "Filtre:\t\tdamier"
+        for y in range(16):
+            for x in range(14):
+                data[y][x] = data[y][x] ^ (y + x) % 2
+        return data
     elif filtre == 2:
-        for y in range(-16, 0, 1):
-            for x in range(-14, 0, 1):
-                matrice[y][x] = matrice[y][x] ^ y % 2
-        return matrice
+        L_info['text'] = "Filtre:\t\tbandes horizontales"
+        for y in range(16):
+            for x in range(14):
+                data[y][x] = data[y][x] ^ y % 2
+        return data
     elif filtre == 3:
-        for y in range(-16, 0, 1):
-            for x in range(-14, 0, 1):
-                matrice[y][x] = matrice[y][x] ^ x % 2
-        return matrice
+        L_info['text'] = "Filtre:\t\tbandes verticales"
+        for y in range(16):
+            for x in range(14):
+                data[y][x] = data[y][x] ^ x % 2
+        return data
 
 
-def ouvrir_fichier():
+def ouvrir_QR():
     f = fd.askopenfile(
         initialdir=os.getcwd() + "/Exemples/",
         title="charger un QR Code",
         filetypes=(("fichier PNG", "*.png"),)
     )
     if f is None:
-        return ""
-    return f.name
+        return
+
+    mat = loading(f.name)
+    mat = verif_sens_QC(mat)
+
+    data = extraire_donnee(mat)
+    filtre = lire_type_filtre(mat)
+    dataType = lire_type_donnee(mat)
+
+    data = applique_filtre(data, filtre)
+    data = lire_donnee(data)
+    interpreter(data, dataType)
+
+#################
+
+
+def to_hamming(string):
+    m1, m2, m3, m4 = map(int, list(string))
+    string += f"{(m1 + m2 + m4) % 2}"
+    string += f"{(m1 + m3 + m4) % 2}"
+    string += f"{(m2 + m3 + m4) % 2}"
+    return list(map(int, list(string)))
+
+
+def del_fen_QR():
+    global fen_QR
+    fen_QR.destroy()
+    del fen_QR
+
+
+def apercu(fen, dataType, filtre, message):
+    data = list(message.get())
+    data = [bin(ord(d))[2:] for d in data]
+    data = ["0" * (8 - len(d)) + d if len(d) != 8 else d for d in data]
+    data = [[to_hamming(d[:4]) + to_hamming(d[4:])] for d in data]
+    n = len(data)
+    for i in range(0, n, 2):
+        m = [[1] * 14 for i in range(16)]
+
+        if i % 4 == 0:
+            for j in range(1, 15):
+                m[i % 2][-((i - 1) // 2 + 1)] = 0
+                m[i % 2][-((i - 1) // 2 + 8)] = 0
+        else:
+            for j in range(1, 15):
+                m[(i + 1) % 2][i // 2] = 0
+                m[(i + 1) % 2][i // 2 + 7] = 0
+    pass
+
+
+def creer_QR(fen, dataType, filtre, message):
+    pass
+
+
+def fen_creer_QR():
+    global root, fen_QR
+    if "fen_QR" in globals():
+        fen_QR.focus_force()
+        return
+
+    fen_QR = tk.Toplevel(root)
+    fen_QR.protocol("WM_DELETE_WINDOW", del_fen_QR)
+    fen_QR.title()
+    fen_QR.resizable(0, 0)
+
+    dataType = tk.IntVar(fen_QR)
+    dataType.set(1)
+    filtre = tk.IntVar(fen_QR)
+    message = tk.StringVar(fen_QR)
+
+    R_ascii = tk.Radiobutton(fen_QR, font=("Ebrima", 12), variable=dataType, text="ASCII", value=1)
+    R_num = tk.Radiobutton(fen_QR, font=("Ebrima", 12), variable=dataType, text="Numérique", value=0)
+
+    R_pas_filtre = tk.Radiobutton(fen_QR, font=("Ebrima", 12), variable=filtre, text="Pas de filtre", value=0)
+    R_damier = tk.Radiobutton(fen_QR, font=("Ebrima", 12), variable=filtre, text="Filtre damier", value=1)
+    R_ligneH = tk.Radiobutton(fen_QR, font=("Ebrima", 12), variable=filtre, text="Lignes Horzontales", value=2)
+    R_ligneV = tk.Radiobutton(fen_QR, font=("Ebrima", 12), variable=filtre, text="Lignes Verticales", value=3)
+
+    L_message = tk.Label(fen_QR, text="Meesage: ")
+    E_message = tk.Entry(fen_QR, textvariable=message)
+
+    B_apercu = tk.Button(fen_QR, text="Aperçu du QR Code", command=lambda: apercu(fen_QR, dataType, filtre, message))
+    B_creer = tk.Button(fen_QR, text="Créer le QR Code", command=lambda: creer_QR(fen_QR, dataType, filtre, message))
+
+    L_image = tk.Label(fen_QR)
+
+    R_ascii.grid(row=0, column=0, columnspan=2)
+    R_num.grid(row=0, column=2, columnspan=2)
+
+    R_pas_filtre.grid(row=1, column=0)
+    R_damier.grid(row=1, column=1)
+    R_ligneV.grid(row=1, column=2)
+    R_ligneH.grid(row=1, column=3)
+
+    L_message.grid(row=2, column=0, columnspan=2)
+    E_message.grid(row=2, column=2, columnspan=2)
+
+    B_apercu.grid(row=3, column=0, columnspan=2)
+    B_creer.grid(row=3, column=2, columnspan=2)
+
+    L_image.grid(row=4, column=0, columnspan=4)
+
+    fen_QR.mainloop()
+#################
 
 
 def main():
+    global root, L_image, L_info
     root = tk.Tk()
-    root.title("vives les damiers :wink:")
-    file = ""
-    while file == "":
-        file = ouvrir_fichier()
-    mat = loading(file)
-    mat = verif_sens_QC(mat)
-    mat = applique_filtre(mat)
-    data = lire_qr_code(mat)
-    interpreter(mat, data)
+    root.title("QR Code")
+
+    B_open_QR = tk.Button(root, text="Lire un QR Code", command=ouvrir_QR)
+    B_open_QR.grid(row=0)
+
+    B_open_QR = tk.Button(root, text="Créer un QR Code", command=fen_creer_QR)
+    B_open_QR.grid(row=0, column=1)
+
+    L_image = tk.Label(root)
+    L_image.grid(row=1, columnspan=2)
+
+    L_infoT = tk.Label(root, text="Info", font=('arial', 15))
+    L_infoT.grid(row=0, column=2)
+
+    L_info = tk.Label(root, height=3, justify='left', font=('arial', 15))
+    L_info.grid(row=1, column=2)
+
+    root.mainloop()
 
 
 main()
